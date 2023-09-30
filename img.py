0ddf3e3b24a39639a1e1802f403b89e90dd215a9
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2023-09-30 09:04:55 +0530

    Add files via upload

---
+++ img.py
@@ -0,0 +1,22 @@
+import cv2    
+import time
+cpt = 0
+maxFrames = 1300 # if you want 5 frames only.
+
+count=0
+cap=cv2.VideoCapture('traffic.mp4')
+while cpt < maxFrames:
+    ret, frame = cap.read()
+#    count += 1
+#    if count % 3 != 0:
+#        continue
+    frame=cv2.resize(frame,(1080,500))
+    time.sleep(0.01)
+#    frame=cv2.flip(frame,1)
+    cv2.imshow("test window", frame) # show image in window
+    cv2.imwrite(r"C:/Users/freed/Downloads/headcount/images\person_%d.jpg" %cpt, frame)
+    cpt += 1
+    if cv2.waitKey(5)&0xFF==27:
+        break
+cap.release()   
+cv2.destroyAllWindows()
